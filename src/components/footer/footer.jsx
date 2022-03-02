59ee0d94906d8694f058ed1ba5c6cc784a6e1c1d
B-SFS <silwadi35@gmail.com>
2022-03-02 08:13:00 +0300

    having an error

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,12 @@
+function Footer()
+{
+return(
+    <>
+     <a href="https://web.facebook.com/">Facebok</a>
+     <a href="https://www.instagram.com/">Insgram</a>
+     <p>&copy Suhaib Silwadi</p>
+    </>
+   
+)
+}
+export default Footer;
